e924e5b9a6f5b3ec6cc462143ee0a5c053496b8f
Flor-Gust <luan.flor05@gmail.com>
2025-03-16 16:36:57 -0300

    SCRUM-52: Buildando a aplicação

---
+++ dist/services/exemploService.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.exemploService = void 0;
+const exemploService = () => {
+    return 'Esta é uma resposta do serviço!';
+};
+exports.exemploService = exemploService;
