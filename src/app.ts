84bfc00936bdba9a0f5e6b5378064931f2c3e47c
Flor-Gust <luan.flor05@gmail.com>
2025-03-22 00:24:36 -0300

    SCRUM-54: Ajuste rota

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import exemploRoutes from './routes/exemploRoutes';
 import dotenv from 'dotenv';
 import usuarioRoutes from './routes/UsuarioRouter';
 
@@ -8,8 +7,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-//app.use('/api', exemploRoutes);
-app.use('/usuario', usuarioRoutes);
+app.use('/', usuarioRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('API está rodando!');
--- src/routes/exemploRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { exemploController } from '../controllers/exemploController';
-
-const router = express.Router();
-
-router.get('/exemplo', exemploController);
-
-export default router;
