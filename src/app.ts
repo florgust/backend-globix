cc701825a7084365e40e521ce3e2cb162eab3913
Flor-Gust <luan.flor05@gmail.com>
2025-03-23 16:00:13 -0300

    SCRUM-56: Ajuste usuario

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import usuarioRoutes from './routes/UsuarioRouter';
 import localizacaoRouter from './routes/LocalizacaoRouter';
 
 dotenv.config();
@@ -8,7 +7,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use('/', usuarioRoutes);
 app.use('/', localizacaoRouter);
 
 app.get('/', (req: Request, res: Response) => {
