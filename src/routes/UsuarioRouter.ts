6475759d13b4d0945f9b1f0826e3a11031b3b00a
Flor-Gust <luan.flor05@gmail.com>
2025-03-23 16:09:51 -0300

    SCRUM-56: Ajuste

---
+++ src/routes/UsuarioRouter.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import {
+    getUsuarios,
+    getUsuarioById,
+    createUsuario,
+    updateUsuario,
+    deleteUsuario
+} from '../controllers/UsuarioController';
+
+const router = Router();
+
+// Rotas para Usuários
+router.get('/usuarios', getUsuarios); // Buscar todos os usuários
+router.get('/usuarios/:id', getUsuarioById); // Buscar um usuário por ID
+router.post('/usuarios', createUsuario); // Criar um novo usuário
+router.put('/usuarios/:id', updateUsuario); // Atualizar um usuário existente
+router.put('/usuarios/:id/delete', deleteUsuario); // Alterar o status de um usuário para 0 (desativar)
+
+export default router;
