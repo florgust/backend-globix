77593da39d4c4943ec55130aebc62479bbcd0844
florgust <luan.flor05@gmail.com>
2025-05-11 17:05:21 -0300

    SCRUM-100: Criando metodo para buscar viagem à partir do codigo convite

--- src/routes/SolicitacaoRouter.ts
+++ src/routes/SolicitacaoRouter.ts
@@ -6,14 +6,13 @@ import {
     atualizarStatusSolicitacao,
     promoverOuDespromoverOrganizadorSolicitacao
 } from '@controllers/api/solicitacao/SolicitacaoController';
-import { authenticateToken } from '@middlewares/AuthenticationToken';  // Certifique-se de importar o middleware
 
 const router = express.Router();
 
 // Rotas para Solicitações
 router.get('/solicitacoes/usuario/:idUsuario', getSolicitacoesPorUsuario); // Buscar todas as solicitações de um usuário
 router.get('/solicitacoes/viagem/:idViagem', getSolicitacoesPorViagem); // Buscar todas as solicitações de uma viagem
-router.post('/solicitacao/:idUsuario/:idViagem', authenticateToken, criarSolicitacao); // Criar uma nova solicitação de viagem
+router.post('/solicitacao/:idUsuario/:idViagem', criarSolicitacao); // Criar uma nova solicitação de viagem
 router.post('/solicitacao/promocao/:idViagem/:idUsuarioOrganizador', promoverOuDespromoverOrganizadorSolicitacao); // Promover ou despromover um organizador de viagem
 router.put('/solicitacao/:idViagem/:idUsuario/status', atualizarStatusSolicitacao); // Atualizar o status de uma solicitação
 
--- src/routes/ViagemRouter.ts
+++ src/routes/ViagemRouter.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {
     getViagens,
     getViagemById,
+    getViagemByCodigoConvite,
     createViagem,
     updateViagem,
     deleteViagem
@@ -11,6 +12,7 @@ const router = express.Router();
 
 router.get('/viagens', getViagens);
 router.get('/viagem/:id', getViagemById);
+router.get('/viagem/codigo/:codigoConvite', getViagemByCodigoConvite);
 router.post('/viagem', createViagem);
 router.put('/viagem/:id', updateViagem);
 router.delete('/viagem/:id', deleteViagem);
